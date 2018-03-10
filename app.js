b6cb4bd43c20c468408cee28688982685b6bb401
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-09 20:27:03 -0800

    solved problem 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
-  
+
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
@@ -58,11 +58,25 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  // first array element
+  var sumOfTwoNums = sum(a, b)[0];
+  var sumOfThreeNums = sum(sumOfTwoNums, c)[0];
+  //console.log(sumOfThreeNums);
 
+  // second array element
+  var productOfTwoNums = multiply(a, b)[0];
+  var productOfThreeNums = multiply(productOfTwoNums, c)[0];
+  //console.log(productOfThreeNums);
+
+  // concatenate the strings
+  var sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThreeNums + '.';
+  var productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfThreeNums + '.';
+
+  return [sumOfThreeNums, productOfThreeNums, sumString, productString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -80,12 +94,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) { //eslint-disable-line
-
+  var newArray = [];
+  newArray[0] = sum(sum(testArray[0], testArray[1])[0], testArray[2])[0];
+  newArray[1] = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + newArray[0] + ' is their sum.';
+  //console.log(newArray);
+  return newArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
